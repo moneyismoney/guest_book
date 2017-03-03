927ad1b89ff556a9fcf1734c949fe52b8f0e52ec
Oleksandr Tymchenko <moneyismoney@gmail.com>
2017-03-03 14:58:10 +0200

    Final fix in adding comments

--- data/comments.php
+++ data/comments.php
@@ -1,1 +1,10 @@
 <?php die('Error, File not found!'); ?>
+1|4444|ccccccccccvvvvvvvvvvvvvvvvv|aaa.ua|3 Mar 2017, 12:30:42 PM|127.0.0.1|0|ffff@ukr.net||
+2|4444|ccccccccccvvvvvvvvvvvvvvvvv|aaa.ua|3 Mar 2017, 12:32:38 PM|127.0.0.1|0|ffff@ukr.net||
+3|4444|ccccccccccvvvvvvvvvvvvvvvvv|aaa.ua|3 Mar 2017, 12:34:39 PM|127.0.0.1|0|ffff@ukr.net||
+4|4444|ccccccccccvvvvvvvvvvvvvvvvv|aaa.ua|3 Mar 2017, 12:50:38 PM|127.0.0.1|0|ffff@ukr.net||
+5|4444|ccccccccccvvvvvvvvvvvvvvvvv|aaa.ua|3 Mar 2017, 12:52:52 PM|127.0.0.1|0|ffff@ukr.net||
+6|4444|ccccccccccvvvvvvvvvvvvvvvvv|aaa.ua|3 Mar 2017, 12:53:28 PM|127.0.0.1|0|ffff@ukr.net||
+7|4444|ccccccccccvvvvvvvvvvvvvvvvv|aaa.ua|3 Mar 2017, 12:54:15 PM|127.0.0.1|0|ffff@ukr.net||
+8|mama|The test comment|ukr.net|3 Mar 2017, 12:55:02 PM|127.0.0.1|0|fffaaaa@gmail.com||
+9|TTTTTTTest|ffffffffffffffff<br /><br />==========<br /><br />==========<br /><br />44444444444|ain.ua|3 Mar 2017, 12:57:07 PM|127.0.0.1|0|ain.ua@ukr.net||
